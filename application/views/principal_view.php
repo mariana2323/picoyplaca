d5ff8292f27805ea62a97c078fd92efbdf357899
mariana23 <anaacosiri@gmail.com>
2020-08-21 15:02:00 -0500

    Configuración de la pantalla inicial de la página, creación y modificación de la vista principal_view.php

---
+++ application/views/principal_view.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mariana23
+ * Date: 8/21/2020
+ * Time: 12:31 PM
+ */
+$estilo = file_get_contents(base_url() . 'css/estilo.css');
+$html = "<style>$estilo</style>";
+$html .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />';
+$html .= '<div>
+              <a href="'.base_url().'" class="btn btn-success">Pico y Placa</a>
+              <a href="#" class="btn btn-success">Restricción vehicular (Pandemia)</a>
+          </div>';
+echo $html;
